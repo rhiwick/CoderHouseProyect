12f9baaf4ab40c15f26ba98e949cc5582506bf28
iSanty <santiago.navalon@hotmail.com>
2022-07-31 22:15:18 -0300

    casi levanto la imagen en el post

--- muro/views.py
+++ muro/views.py
@@ -6,25 +6,23 @@ from . import forms
 from datetime import datetime
 from django.views.generic.list import ListView
 
-# Create your views here.
+
+
 class Muro(ListView):
     model = models.PosteosUsuarios
     template_name = 'Muro/base.html'
     
     def get_queryset(self):
-        posteos = self.request.GET.get('id', '')
-        if posteos:
-            object_list = self.model.objects.filter(id__icontains=posteos)
-        else:
-            object_list = self.model.objects.all()
-        print(object_list)
+        
+        object_list = self.model.objects.all()
+        
         return object_list
 
     
 @login_required
 def postear(request):
     user = request.user
-    datos_post, _ = models.PosteosUsuarios.objects.get_or_create(autor=user)
+    datos_post = models.PosteosUsuarios()
     
     if request.method == 'POST':
         form = forms.FormCrearPosteo(request.POST, request.FILES)
@@ -37,7 +35,6 @@ def postear(request):
             datos_post.fecha_posteo = datetime.now()
             datos_post.autor = user.username
             datos_post.save()
-            user.save()
             return redirect('muro')
         else:
             return render(request,'Muro/crear_post.html',{'form':form})

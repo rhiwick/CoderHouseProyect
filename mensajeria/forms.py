b692a166a151c0d8ecf33a0dab6874cb54743c62
iSanty <santiago.navalon@hotmail.com>
2022-08-29 23:03:38 -0300

    retoques

--- mensajeria/forms.py
+++ mensajeria/forms.py
@@ -1,8 +1,5 @@
-from secrets import choice
 from django import forms
 from ckeditor.fields import RichTextFormField
-from django.apps import apps
-from django.conf import settings
 from django.contrib.auth.models import User
 from .models import *
 
@@ -11,6 +8,4 @@ class FormCrearMensaje(forms.Form):
     
     a_user = forms.ModelChoiceField(queryset= User.objects.all())
     mensaje = RichTextFormField()
-
-
 
